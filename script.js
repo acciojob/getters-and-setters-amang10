d0a1ccb21c85391c0493966a4b49b5dcbf12a281
amang10 <90174464+amang10@users.noreply.github.com>
2025-04-25 10:27:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -31,3 +31,8 @@ class Teacher extends Person {
     console.log(${this.name} is teaching);
   }
 }
+
+// Do not change the code below this line
+window.Person = Person;
+window.Student = Student;
+window.Teacher = Teacher;
